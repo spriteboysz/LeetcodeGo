d3cf591ad93f94b7366ceb146f3d378a2243ae1d
spriteboysz <sprite_bx@126.com>
2022-10-10 23:44:25 +0800

    2022101023

---
+++ algorithm/P2396. 严格回文的数字.go
@@ -0,0 +1,18 @@
+/**
+ * Author: Deean
+ * Date: 2022-10-10 23:42
+ * FileName: algorithm/P2396. 严格回文的数字.go
+ * Description:
+ */
+
+package main
+
+import "fmt"
+
+func isStrictlyPalindromic(n int) bool {
+	return false
+}
+
+func main() {
+	fmt.Println(isStrictlyPalindromic(5))
+}
